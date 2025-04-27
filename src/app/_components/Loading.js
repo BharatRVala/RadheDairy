524cccaddd1843baab05240995125316816b8f7c
Bharat vala <bharatkumarramashibhai2003@gmail.com>
2025-04-27 20:24:51 +0530

    Initial commit

--- src/app/_components/Loading.js
+++ src/app/_components/Loading.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import Image from 'next/image'
 
 function Loading() {
   return (
     <div className="gif-loader">
-      <img src="/loader.gif" alt="Loading..." className="spinner-img" />
+      <Image
+        src="/loader.gif"
+        alt="Loading..."
+        className="spinner-img"
+        width={100} // Adjust width as needed
+        height={100} // Adjust height as needed
+      />
     </div>
   )
 }
